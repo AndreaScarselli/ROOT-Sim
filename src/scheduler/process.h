1aed657957618604a28bde47c2fc03f4195630a2
Andrea Scarselli <andreascarse@gmail.com>
2015-07-31 13:03:09 +0200

    rimetto in ordine i casini successi con git

--- src/scheduler/process.h
+++ src/scheduler/process.h
@@ -61,25 +61,6 @@
 #define is_blocked_state(state)	(bool)(state & BLOCKED_STATE)
 
 
-
-#define IN_USE_FLAG 0x80000000
-
-typedef struct _ingoing_buffer_element{
-	//header.. MRB is 1 in use or 0 free
-	unsigned int h_size;
-	char* base;
-	unsigned int f_size;
-}ingoing_buffer_element;
-
-typedef struct _ingoing_buffer{
-	char* base;
-	ingoing_buffer_element* first_block;
-	int	offset;
-	int size;
-	spinlock_t lock;
-}ingoing_buffer;
-
-
 typedef struct _LP_state {
 
 	/// Local ID of the thread (used to translate from bound LPs to local id)
